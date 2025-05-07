0b8658bce24beea3cf33829a11c86d5785e56f5a
Georgiy <goshakovalev5@gmail.com>
2025-05-07 16:56:52 +0400

    Header file for list.c

---
+++ tools/list.h
@@ -0,0 +1,15 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct list {
+	char * name;
+	struct list * next;
+} LIST;
+
+void init(LIST **);
+void push(LIST ** root, char * name);
+char * pop(LIST ** root);
+void destroy(LIST **);
+void printList(LIST *);
+
+#endif
