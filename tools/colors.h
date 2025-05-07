0fb3ad52cefabc34948c8adcae5d22e60bfc326a
Georgiy <goshakovalev5@gmail.com>
2025-05-07 16:55:04 +0400

    Header file with colors

---
+++ tools/colors.h
@@ -0,0 +1,21 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define ESC "\033[0m"
+#define HARD_LIGHT "\033[1m"
+#define BLACK "\033[30m"
+#define WHITE "\033[97m"
+#define GREY "\033[90m"
+#define BLUE "\033[94m"
+#define PURPLE "\033[95m"
+#define ORANGE "\033[33m"
+#define GREEN "\033[92m" 
+#define RED "\033[31m"
+#define RED_FRONT "\033[101m"
+#define BLUE_FRONT "\033[46m" // темно-голубой
+#define ORANGE_FRONT "\033[103m"
+#define GREY_FRONT "\033[100m"
+#define WHITE_FRONT "\033[107m"
+#define BLACK_FRONT "\033[40m"
+
+#endif
