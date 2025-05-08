70475b079a18775bfb989f5cdd3689b4868d562e
Georgiy <goshakovalev5@gmail.com>
2025-05-08 21:16:50 +0400

    Header file with utility flags

--- s21_grep_flags.h
+++ s21_grep_flags.h
@@ -1,18 +1,16 @@
 #ifndef S21_GREP_FLAGS_H
 #define S21_GREP_FLAGS_H
 
-	#define GNU_FLAG_E "regexp"				// шаблон
+#define GNU_FLAG_E "regexp"					// шаблон
 #define GNU_FLAG_I "ignore-case" 			// игнорирование регистра
-	#define GNU_FLAG_V "invert-match"		// ищем несовпадения
+#define GNU_FLAG_V "invert-match"			// ищем несовпадения
 #define GNU_FLAG_C "count"					// количество совпадений
-	#define GNU_FLAG_L "files-with_match"	// выводит только файл (h -> 0)
-	#define GNU_FLAG_N "line-number"		// выводить номер строки
+#define GNU_FLAG_L "files-with_match"		// выводит только файл (h -> 0)
+#define GNU_FLAG_N "line-number"			// выводить номер строки
 #define GNU_FLAG_H "no-filename"			// не выводить имя файла
 #define GNU_FLAG_S "no-messages"			// без вывода ошибок
 #define GNU_FLAG_O "only-matching"			// v -> 0, вывод только строки
 #define GNU_FLAG_F "file"					// файл, откуда нужно читать данные
-
-// -vo = empty
 
 #define FLAGS "eivclnhsofP"
 #define FLAGS_OPT "e:ivclnhsof:"
